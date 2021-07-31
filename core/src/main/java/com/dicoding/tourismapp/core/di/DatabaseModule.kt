977c612448c04e4df88292826415a99da78b260d
ariefzuhri <ariefzuhri72@gmail.com>
2021-07-31 08:43:54 +0700

    Codelab enkripsi database

--- core/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
+++ core/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,11 +19,16 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): TourismDatabase =
-        Room.databaseBuilder(
+    fun provideDatabase(@ApplicationContext context: Context): TourismDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
             context,
             TourismDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun provideTourismDao(database: TourismDatabase): TourismDao = database.tourismDao()
